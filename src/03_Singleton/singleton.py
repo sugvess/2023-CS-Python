94bbdfe8070a4c0f1268b1e5bc097211fa6f844c
sugvess <liliannagogoleva@gmail.com>
2023-12-14 13:21:38 +1000

    Add singleton task

--- src/03_Singleton/singleton.py
+++ src/03_Singleton/singleton.py
@@ -1,2 +1,16 @@
+from typing import Optional, Type, Union
+
+
 class Singleton:
-    pass
+    _instance: Optional["Singleton"] = None
+
+    def __new__(cls) -> "Union[Type[Singleton], Singleton]":
+        if not cls._instance:
+            print("Create Singleton")
+            cls._instance = super().__new__(cls)
+            cls._instance.__init__()
+        print("Return Singleton")
+        return cls._instance
+
+    def __init__(self):
+        print("Init Singleton")
